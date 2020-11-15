0fe5d6869b3a071aa4d084fbd6aac45fdef01f7d
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-11-15 13:03:35 +0100

    Sharedpreferences storing the app opened somewhen.

--- app/src/main/java/com/codecool/applicationa/MainActivityPresenter.kt
+++ app/src/main/java/com/codecool/applicationa/MainActivityPresenter.kt
@@ -1,6 +1,12 @@
 package com.codecool.applicationa
 
+import android.content.SharedPreferences
+
 class MainActivityPresenter {
+
+    companion object {
+        private val IS_APP_OPENED_SOMEWHEN : String = "com.codecool.plantapp.is_app_opened"
+    }
 
     private var view : MainActivityPresenterView? = null
 
@@ -8,9 +14,19 @@ class MainActivityPresenter {
         this.view = view
     }
 
-    fun onDetachView ( view : MainActivityPresenterView){
+    fun onDetachView (){
         this.view = null
     }
 
+    fun isAppOpenedSomewhen(sharedPreferences: SharedPreferences) {
 
+        val result = sharedPreferences.getBoolean(IS_APP_OPENED_SOMEWHEN,false)
+        if ( !result) {
+            sharedPreferences.edit()
+                .putBoolean(IS_APP_OPENED_SOMEWHEN,true)
+                .apply()
+        }
+        view?.isAppOpenedReciever(result)
+
+    }
 }
