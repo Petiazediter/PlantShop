f62e77c644bd328945d5bd3f351304d61437faba
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-11-15 15:21:08 +0100

    Login and sign in navigation + design fixes

--- app/src/main/java/com/codecool/applicationa/sign_up_screen/SignUpFragment.kt
+++ app/src/main/java/com/codecool/applicationa/sign_up_screen/SignUpFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.codecool.applicationa.R
+import kotlinx.android.synthetic.main.fragment_sign_in.*
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 
 class SignUpFragment : Fragment() {
@@ -18,5 +21,12 @@ class SignUpFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        to_login.setOnClickListener {
+            findNavController()
+                .navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+    }
 
 }
