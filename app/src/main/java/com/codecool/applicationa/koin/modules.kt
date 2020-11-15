2d3463549f909c4b12d38d37203747f884a913d0
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-11-15 16:10:16 +0100

    Added koin files to the manifest.

--- app/src/main/java/com/codecool/applicationa/koin/modules.kt
+++ app/src/main/java/com/codecool/applicationa/koin/modules.kt
@@ -1,7 +1,9 @@
 package com.codecool.applicationa.koin
 
+import com.codecool.applicationa.koin.database_sign_up.KoinSignUp
+import com.codecool.applicationa.koin.database_sign_up.SignUpService
 import org.koin.dsl.module
 
 val modules = module{
-    
+    single<SignUpService>{KoinSignUp()}
 }
