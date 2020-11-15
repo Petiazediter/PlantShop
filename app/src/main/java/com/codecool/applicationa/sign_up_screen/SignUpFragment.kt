3a591813cc94126a383d02801a7aaca910dbb4cc
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-11-15 16:29:00 +0100

    Added button listener to sign up

--- app/src/main/java/com/codecool/applicationa/sign_up_screen/SignUpFragment.kt
+++ app/src/main/java/com/codecool/applicationa/sign_up_screen/SignUpFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.codecool.applicationa.R
 import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 
-class SignUpFragment : Fragment() {
+class SignUpFragment : Fragment(), SignUpContractor {
+
+    private val presenter = SignUpPresenter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +29,33 @@ class SignUpFragment : Fragment() {
             findNavController()
                 .navigate(R.id.action_signUpFragment_to_signInFragment)
         }
+
+        presenter.onAttach(this)
+
+        sign_up_button.setOnClickListener {
+            val username = username.text.toString()
+            val password1 = password.text.toString()
+            val password2 = password2.text.toString()
+            val email = email.text.toString()
+            presenter.attemptRegister(username,password1,password2,email)
+            sign_up_button.isEnabled = false
+        }
     }
 
+    override fun onError(messageId: Int) {
+        sign_up_button.isEnabled = true
+        context?.let{
+            Toast.makeText(it,resources.getString(messageId),Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+    override fun onSuccess() {
+        sign_up_button.isEnabled = true
+        context?.let{
+            Toast.makeText(it,resources.getString(R.string.success_register),Toast.LENGTH_LONG)
+                .show()
+        }
+        findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
+    }
 }
